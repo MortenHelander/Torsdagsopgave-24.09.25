f073c201db825070c50be725f72fbed01ecab809
MortenHelander <morten.helander@hotmail.com>
2025-09-25 13:52:28 +0200

    Task4 complete

--- Torsdagsopgaver 24.09/Task4/GameMenu.java
+++ Torsdagsopgaver 24.09/Task4/GameMenu.java
@@ -4,14 +4,13 @@ public class GameMenu{
 	
 	private ArrayList<String>actions=new ArrayList<>();
 
+	//Constructor.
 	GameMenu(ArrayList<String>actions){
 	this.actions=actions;
-
-
-
-
 	}
 
+
+	//Show menu method.
 	public void displayMenu(){
 		for(String e : actions){
 			System.out.println(e);
--- Torsdagsopgaver 24.09/Task4/Main.java
+++ Torsdagsopgaver 24.09/Task4/Main.java
@@ -3,16 +3,16 @@ import java.util.ArrayList;
 public class Main{
 	
 	public static void main(String[]args){
-		GameMenu g = new GameMenu(ArrayList<GameMenu>actions);
-
+		//Create ArrayList and make an object.
 		ArrayList<String>actions=new ArrayList<>();
+		GameMenu g = new GameMenu(actions);
 
+		
+		//Add menu options.
 		actions.add("1) Start game");
 		actions.add("2) Resume game");
 		actions.add("3) Pause game");
 		actions.add("4) End game");
-
-		//System.out.print(actions.get(2)); // expected output: "Pause game"
 	
 
 		g.displayMenu();
